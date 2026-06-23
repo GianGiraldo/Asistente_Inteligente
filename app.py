d3fd5944988205bdc99f6d9937617f9426a19d6b
GianGiraldo <gianpiergiraldo@gmail.com>
2026-06-23 12:22:46 -0500

    Cambiar favicon por logo veloX

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="veloX",
-    page_icon="⚡",
+    page_icon="assets/velox.png",
     layout="wide",
     initial_sidebar_state="expanded",  # Obliga sidebar abierta al cargar (también en móvil)
 )
